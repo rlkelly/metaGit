4ff00aad99c4342c0dd0d014b20c24d241f3b4d4
Robert L Kelly <robert.l.kelly3@gmail.com>
2016-02-13 14:11:22 -0800

    fixup

--- github.py
+++ github.py
@@ -1,1 +1,8 @@
+import os
+
 # access github repo
+
+rv = requests.get(url='http://52.3.253.34:80/occm/api/vsa/volumes?workingEnvironmentId=VsaWorkingEnvironment-CASrF978',
+                  headers={'Cookie': 'JSESSIONID=1gnm17xx03t1y1qkekk3s4x8fv'})
+
+curl 'https://api.github.com/repos/rlkelly/metaGit/branches?client_id=d501ec761e2427f67ccc&client_secret={}'.format(os.environ['client_secret'])
