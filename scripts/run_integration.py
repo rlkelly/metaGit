d819203936c240013f72d09198ce4e1e4b2343a3
Robert L Kelly <robert.l.kelly3@gmail.com>
2016-02-13 19:51:24 -0800

    added integration

--- scripts/run_integration.py
+++ scripts/run_integration.py
@@ -1,6 +1,5 @@
-import os
+#!/bin/bash
+alias home="cd"
 
-os.system('ssh -i ~/.ssh/devweek.pem ec2-user@ec2-52-3-253-34.compute-1.amazonaws.com')
-os.system('yes')
-os.system('echo')
-os.system('ls')
+home
+sudo mount -o nolock  172.31.44.3:/"$1" /home/ec2-user/metaGit
