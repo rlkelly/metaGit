4e5c8ef6fabf065dd78bd2b0aea5ed7a2bde2aca
Robert L Kelly <robert.l.kelly3@gmail.com>
2016-02-13 19:39:54 -0800

    simplified

--- scripts/repo_tree.sh
+++ scripts/repo_tree.sh
@@ -2,7 +2,6 @@
 alias home="cd"
 alias metaGit="cd metaGit"
 
-echo "$1"
 curl 'http://52.3.253.34/occm/api/vsa/volumes?createAggregateIfNotFound=false' -H 'Origin: http://52.3.253.34' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Referer: http://52.3.253.34/occm/api-doc/' -H 'Cookie: JSESSIONID=1gnm17xx03t1y1qkekk3s4x8fv' -H 'Connection: keep-alive' --data-binary $'{\n  "workingEnvironmentId": "VsaWorkingEnvironment-CASrF978",\n  "svmName": "svm_First_Instance",\n  "aggregateName": "aggr1",\n  "name": "'"$1"'",\n  "size": {\n    "size": "10",\n    "unit": "GB"\n  },\n  "initialSize": {\n    "size": "1",\n    "unit": "GB"\n  },\n  "snapshotPolicyName": "default",\n  "exportPolicyInfo": {\n    "policyType": "custom",\n    "ips": [\n      "172.31.0.0/16"\n    ]\n  },\n  \n  "enableThinProvisioning": true,\n  "enableCompression": false,\n  "enableDeduplication": true,\n  "maxNumOfDisksApprovedToAdd": 1\n}' --compressed
 
 for var in "${@: 2:999999}"
@@ -13,11 +12,7 @@ do
     home
     sudo mount -o nolock  172.31.44.3:/$var /home/ec2-user/metaGit
     metaGit
-    git clone git@github.com:rlkelly/metaGit.git
-    metaGit
     git checkout $var
     home
     sudo umount /home/ec2-user/metaGit
 done
-
-sudo umount /home/ec2-user/metaGit
